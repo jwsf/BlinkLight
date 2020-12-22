90ff987c76cc68f628b86f307e95c5b51598f452
Jonathan Wilson <jowilson@zendesk.com>
2020-12-22 12:34:02 -0800

    Simplified and changed so it supports switches

--- blinklight.groovy
+++ blinklight.groovy
@@ -12,8 +12,8 @@
  *
  *
  *  Blink Light:
- *		Blink a light when motion is detected or  open/close sensor opens or closes then reset light to original state.
- *	Optionally, send blink SOS when motion is detected.  
+ *    Blink a light when motion is detected or  open/close sensor opens or closes then reset light to original state.
+ *  Optionally, send blink SOS when motion is detected.  
  *
  *  Author: Tom Young, blinklight@twyoung.com, 11/12/'18
  */
@@ -28,53 +28,54 @@ definition(
 )
 
 preferences {
-	section("When there is motion or a contact change, blink a light up to ten seconds.  Optionally, blink S.O.S if motion detected.") {
-		input "motion1", "capability.motionSensor", title: "What motion?", multiple: true,required: false 
-	}
-	section("Blink a light when this contact opens or closes.") {
-		input "contact1", "capability.contactSensor", title: "What contact?",multiple: true,required: false
-	}
-	section("Blink this light, up to ten seconds.") {
-		input "switch1", "capability.switch", title: "Which light?",required: true
-	}
-	section("Light timing for motion or open contact.") {
-		input "nblinks", "number", title: "Number of Blinks?\nWARNING: Too high a number may exceed ST limits.", required: true,defaultValue: 5
-		input "mson", "number", title: "Milliseconds light on?", required: true,defaultValue: 100
-		input "msoff", "number", title: "Milliseconds light off?", required: true,defaultValue: 100
-	}
-	section("Light timing for closed contact or SOS signal.") {
-		input "cnblinks", "number", title: "Number of Blinks? If over 1000, motion will issue S.O.S..", required: true,defaultValue: 0
-		input "cmson", "number", title: "Milliseconds light on or S.O.S. ms timing unit?", required: true,defaultValue: 100
-		input "cmsoff", "number", title: "Milliseconds light off?", required: true,defaultValue: 102
-	}
+  section("When there is motion or a contact change, blink a light up to ten seconds.  Optionally, blink S.O.S if motion detected.") {
+    input "motion1", "capability.motionSensor", title: "What motion?", multiple: true,required: false 
+  }
+  section("Blink a light when this contact opens or closes.") {
+    input "contact1", "capability.contactSensor", title: "What contact?",multiple: true,required: false
+  }
+    section("Blink a light when this switch turns on.") {
+    input "switch_sensor", "capability.switch", title: "which switch?",multiple: true,required: false
+  }
+  section("Blink this light, up to ten seconds.") {
+    input "switch1", "capability.switch", title: "Which light?",required: true
+  }
+  section("Light timing") {
+    input "nblinks", "number", title: "Number of Blinks?\nWARNING: Too high a number may exceed ST limits.", required: true,defaultValue: 5
+    input "mson", "number", title: "Milliseconds light on?", required: true,defaultValue: 100
+    input "msoff", "number", title: "Milliseconds light off?", required: true,defaultValue: 100
+  }
 }
 
 def installed() {
-	subscribe(motion1, "motion.active", motionActive)
-	subscribe(contact1, "contact", blinker)
+  subscribe(motion1, "motion.active", motionActive)
+  subscribe(contact1, "contact", blinker)
+    subscribe(switch_sensor, "switch.on", blinker)
 }
 
 def updated() {
-	unsubscribe()
-	subscribe(motion1, "motion.active", motionActive)
-	subscribe(contact1, "contact", blinker)
+  unsubscribe()
+  subscribe(motion1, "motion.active", motionActive)
+  subscribe(contact1, "contact", blinker)
+    subscribe(switch_sensor, "switch.on", blinker)
 }
 
    
 def turnOff(int ms) {
-        switch1.off()
-       	pause(ms)
+    switch1.off()
+    pause(ms)
 }
 
 def turnOn(int ms) {
-	switch1.on() 
-  	pause(ms)
+  switch1.on() 
+    pause(ms)
 }
 
 def timeOK(int nblinks, int mson, int msoff) {
 
+  log.debug "Total flash duration is: " + (nblinks * (mson + msoff))
    if(  nblinks * (mson + msoff) < 10000 ) {     /** Maximum Ten second time limit */
-   		return 1
+      return 1
    }
    
    log.error "Blink time exceeds ten seconds"
@@ -84,22 +85,22 @@ def timeOK(int nblinks, int mson, int msoff) {
 def resetSwitch( String swval) {
 
    if(swval == "on") {
-   		switch1.on()
+      switch1.on()
         log.debug " resetSwitch:"     
    } else {
    if(swval == "off") {
-   		log.debug " resetSwitch: sw is off"
+      log.debug " resetSwitch: sw is off"
    } else {
    if(swval == "null") {
-   		log.debug "resetSwitch: sw is null"
+      log.debug "resetSwitch: sw is null"
    } else {    
-   		log.error " resetSwitch: swval=$swval is not on or off" 
+      log.error " resetSwitch: swval=$swval is not on or off" 
    }}}
    
 }   
    
 def blinkLight(int n, int mson, int msoff) {
-	int count=0
+  int count=0
     
    def swval = switch1.currentSwitch
    log.debug "blinkLight/swval: $swval"
@@ -107,11 +108,11 @@ def blinkLight(int n, int mson, int msoff) {
    log.debug "swval instanceof Number? ${swval instanceof Number}"
    
    if (timeOK(nblinks,mson,msoff)) {     /** Maximum Ten second time limit */
-    	while(count<n) {
+      while(count<n) {
             turnOn(mson) 
-        	turnOff(msoff)
+          turnOff(msoff)
             count++
-    	}
+      }
    }
         
    resetSwitch(swval)  
@@ -119,39 +120,16 @@ def blinkLight(int n, int mson, int msoff) {
 }
 
 def blinker(evt) {     /* Note twenty second limit on SmartApps.  10 second limit here.  */ 
-    
-	log.debug "blinker: $evt.value"
-	if (evt.value == "open") {
-    	blinkLight(nblinks,mson,msoff)
-	} else if (evt.value == "closed") { 
-    	if(cnblinks<1001) {
-    		blinkLight(cnblinks,cmson,cmsoff)
-        }    
-	}
+  log.debug "blinker: $evt.value"
+  if ((evt.value == "open") || (evt.value == "on")) {
+      blinkLight(nblinks,mson,msoff)
+  } else if (evt.value == "closed") { 
+      blinkLight(cnblinks,cmson,cmsoff)  
+  }
 }
 
 def motionActive(evt) {
-
-	log.debug "Motion: $evt.value"
-    if(cnblinks>1000) {
-    	sendSOS()
-    } else {    
-		blinkLight(nblinks,mson,msoff)
-    }    
+  log.debug "Motion: $evt.value"   
+  blinkLight(nblinks,mson,msoff)  
 }
   
-def sendSOS() {
-	def swval = switch1.currentSwitch
-	log.debug "sendSOS tu=$cmson swval=$swval"
-    if(swval == "on") {
-        turnOff(7*cmson)  /* seven time units between words. For five words/minute set Morse timing unit(cmson) = 240ms.*/
-    }
-    def dit = { turnOn(cmson);   turnOff(cmson); }
-    def dah = { turnOn(3*cmson); turnOff(cmson); }
-    def ics = { pause(2*cmson) } /* Inter character spacing is three time units, but one unit has elapsed already */
-    dit(); dit(); dit(); ics()
-    dah(); dah(); dah(); ics()
-    dit(); dit(); dit() 
-    pause(6*cmson)   
-    resetSwitch(swval) 
-}
